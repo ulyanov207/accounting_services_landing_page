53d204186cab2ccfb81643f9019e65477ce93d12
ulyanov207 <ulyanov207@gmail.com>
2022-05-12 17:19:12 +0300

    2 commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,36 @@
 import React from 'react';
-import './Header.module.css'
+import s from './Header.module.css'
 import logo from "../../img/svg/logo.svg";
 import BuhOne from "../../img/svg/BuhOne.svg";
 
 const Header = () => {
     return (
-            <div className="header">
-                <div className="header_logo">
-                    <a href="/" className="header_logo-link">
-                    <img src={logo} alt="logo" className="header_logo-pic"/>
-                        <img src={BuhOne} alt="BuhOne" className="header_BuhOne-pic"/>
+            <div className={s.header}>
+                <div className={s.headerLogo}>
+                    <a href="/" className={s.header_logo_link}>
+                    <img src={logo} alt="logo" className={s.header_logo_pic}/>
+                        <img src={BuhOne} alt="BuhOne" className={s.header_BuhOne_pic}/>
                     </a>
                 </div>
+                <nav className={s.header_nav}>
+                    <ul className={s.header_list}>
+                        <li className={s.header_item}>
+                            <a href="#!" className={s.header_link}>Главная</a>
+                        </li>
+                        <li className="header_item">
+                            <a href="#!" className={s.header_link}>услуги</a>
+                        </li>
+                        <li className="header_item">
+                            <a href="#!" className={s.header_link}>кейсы</a>
+                        </li>
+                        <li className="header_item">
+                            <a href="#!" className={s.header_link}>о компании</a>
+                        </li>
+                        <li className="header_item">
+                            <a href="#!" className={s.header_link}>контакты</a>
+                        </li>
+                    </ul>
+                </nav>
             </div>
     );
 };
