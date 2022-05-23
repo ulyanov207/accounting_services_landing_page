13f46977543671592901f3becb45018cc1a3b3c2
ulyanov207 <ulyanov207@gmail.com>
2022-05-23 18:47:27 +0300

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,35 +1,51 @@
 import React from 'react';
 import s from './Header.module.css'
-import logo from "../../img/svg/logo.svg";
-import BuhOne from "../../img/svg/BuhOne.svg";
+import logo from "../../img/svg/header/logo.svg";
+import BuhOne from "../../img/svg/header/BuhOne.svg";
+import phone from "../../img/svg/header/phone.svg"
+import room_phone from "../../img/svg/header/room_phone.svg"
+import watch from "../../img/svg/header/watch.svg"
+import office_hours from "../../img/svg/header/office_hours.svg"
+import mail from "../../img/svg/header/mail.svg"
+import mail_address from "../../img/svg/header/mail_address.svg"
+import location from "../../img/svg/header/location.svg"
+import address from "../../img/svg/header/address.svg"
 
 const Header = () => {
     return (
-            <div className={s.header}>
-                <div className={s.header_logo}>
-                    <img src={logo} alt="logo" className={s.header_logo_pic}/>
-                        <img src={BuhOne} alt="BuhOne"/>
-                </div>
-                <nav className={s.header_nav}>
-                    <ul className={s.header_list}>
-                        <li className={s.header_item}>
-                            <a href="#!" className={s.header_link}>Главная</a>
-                        </li>
-                        <li className={s.header_item}>
-                            <a href="#!" className={s.header_link}>услуги</a>
-                        </li>
-                        <li className={s.header_item}>
-                            <a href="#!" className={s.header_link}>кейсы</a>
-                        </li>
-                        <li className={s.header_item}>
-                            <a href="#!" className={s.header_link}>о компании</a>
-                        </li>
-                        <li className={s.header_item}>
-                            <a href="#!" className={s.header_link}>контакты</a>
-                        </li>
-                    </ul>
-                </nav>
+        <div className={s.header}>
+            <div className={s.header_logo}>
+                <img src={logo} alt="logo" className={s.header_logo_pic}/>
+                <img src={BuhOne} alt="BuhOne"/>
             </div>
+            <div className={s.contact}>
+                <div><img src={phone} alt="phone" className={s.header_phone}/></div>
+                <div><img src={room_phone} alt="room_phone" className={s.header_room}/></div>
+                <div><img src={watch} alt="watch" className={s.header_watch}/></div>
+                <div><img src={office_hours} alt="office_hours"/></div>
+                <div><img src={mail} alt="mail" className={s.header_mail}/></div>
+                <div><img src={mail_address} alt="mail_address" className={s.header_mail_address}/></div>
+                <div><img src={location} alt="location" className={s.header_location}/></div>
+                <div><img src={address} alt="address" className={s.header_address}/></div>
+            </div>
+            <ul className={s.header_nav}>
+                <li className={s.header_item}>
+                    <a href="#!" className={s.header_link}>Главная</a>
+                </li>
+                <li className={s.header_item}>
+                    <a href="#!" className={s.header_link}>услуги</a>
+                </li>
+                <li className={s.header_item}>
+                    <a href="#!" className={s.header_link}>кейсы</a>
+                </li>
+                <li className={s.header_item_c}>
+                    <a href="#!" className={s.header_link}>о компании</a>
+                </li>
+                <li className={s.header_item}>
+                    <a href="#!" className={s.header_link}>контакты</a>
+                </li>
+            </ul>
+        </div>
     );
 };
 
