582fa1e2050352003388e26881fea57012f7935b
ulyanov207 <ulyanov207@gmail.com>
2022-06-20 21:37:31 +0300

    New-Presentation_Services

--- src/components/Presentation/Presentation.jsx
+++ src/components/Presentation/Presentation.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import s from './Presentation.module.css'
 import Rectangle from "./../../img/Rectangle.jpg";
 import Ellipse from "../../img/svg/Presentation/Ellipse.svg"
@@ -7,24 +7,40 @@ import Vector2 from "../../img/svg/Presentation/Vector2.svg"
 
 
 const Presentation = () => {
+
+    // менее используемый аналог media-queres
+    const [mobile, setMobile] = React.useState(false)
+
+    useEffect(() => {
+        if (window && window.innerWidth < 1200){
+            setMobile(true)
+        }
+    }, [])
+
     return (
-        <div className={s.intro}>
-            <img src={Rectangle} alt="Rectangle" className={s.intro_img}/>
-            <h1>Бухгалтерские услуги<br/>в Санкт-Петербурге</h1>
-            <button className={s.presentation}>Наша презентация</button>
-            <div>
-                <div><img src={Ellipse} className={s.slider_indicators1}/></div>
-                <div><img src={Ellipse} className={s.slider_indicators2}/></div>
-                <div><img src={Ellipse} className={s.slider_indicators_active}/></div>
-                <div><img src={Ellipse} className={s.slider_indicators4}/></div>
+            <div className={s.intro}>
+                <img src={Rectangle} alt="Rectangle" className={s.intro_img}/>
+                <div className={s.presentationContainer}>
+                    <div className={s.presentationContainerLeft}>
+                        <h1 className={s.presentationServices}>Бухгалтерские услуги<br/>в Санкт-Петербурге</h1>
+                        <button className={s.presentation}>Наша презентация</button>
+                        <div className={s.slider_indicators}>
+                            <div><img src={Ellipse} className={s.slider_indicators_inactive}/></div>
+                            <div><img src={Ellipse} className={s.slider_indicators_inactive}/></div>
+                            <div><img src={Ellipse} className={s.slider_indicators_active}/></div>
+                            <div><img src={Ellipse} className={s.slider_indicators_inactive}/></div>
+                        </div>
+                    </div>
+                    <div className={s.presentationContainerRight}>
+                        <a className={s.slider_control_prev} href="#!" role="button">
+                            <div><img src={Vector1}/></div>
+                        </a>
+                        <a className={s.slider_control_next} href="#!" role="button">
+                            <div><img src={Vector2}/></div>
+                        </a>
+                    </div>
+                </div>
             </div>
-            <div>
-                <a className={s.slider_control_prev} href="#!" role="button"></a>
-                <a className={s.slider_control_next} href="#!" role="button"></a>
-                <div><img src={Vector1} className={s.vector1}/></div>
-                <div><img src={Vector2} className={s.vector2}/></div>
-            </div>
-        </div>
     );
 };
 
