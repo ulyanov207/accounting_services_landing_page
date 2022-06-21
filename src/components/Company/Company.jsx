71511fc5e32d373e43f46ca1a21ae0ef3107eb06
ulyanov207 <ulyanov207@gmail.com>
2022-06-21 19:37:27 +0300

    New-Company

--- src/components/Company/Company.jsx
+++ src/components/Company/Company.jsx
@@ -4,16 +4,18 @@ import s from './Company.module.css'
 const Company = () => {
     return (
         <div className={s.wrapper}>
-            <div className={s.text}>О нас</div>
-            <div className={s.company}>
-                <span>Компания</span>
-                <span className={s.company_ivanov}><br/>ИвановПром</span>
-            </div>
-            <div className={s.text_lorem}>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне.
-                Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века. В то время некий
-                безымянный печатник создал большую коллекцию размеров и форм шрифтов, используя Lorem Ipsum для
-                распечатки образцов. Lorem Ipsum не только успешно пережил без заметных изменений пять веков, но и
-                перешагнул в электронный дизайн.
+            <div className={s.aboutUs}>
+                <div className={s.text}>О нас</div>
+                <div className={s.company}>
+                    <span>Компания</span>
+                    <span className={s.company_ivanov}><br/>ИвановПром</span>
+                </div>
+                <div className={s.text_lorem}>Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне.
+                    Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века. В то время некий
+                    безымянный печатник создал большую коллекцию размеров и форм шрифтов, используя Lorem Ipsum для
+                    распечатки образцов. Lorem Ipsum не только успешно пережил без заметных изменений пять веков, но и
+                    перешагнул в электронный дизайн.
+                </div>
             </div>
             <div className={s.wrapper_block}>
                 <div className={s.block}>
